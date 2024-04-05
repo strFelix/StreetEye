afba530caa3077b320f2e855926f4c7193ac33b6
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2024-04-05 01:02:52 -0300

    feat(semaforo): add hasData migration

---
+++ StreetEye.api/Migrations/20240405035907_addDefaultSemaforo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreetEye.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDefaultSemaforo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "semaforos",
+                columns: new[] { "Id", "Descricao", "Endereco", "IntervaloAberto", "IntervaloFechado", "Latitude", "Longitude", "Numero", "ViaCruzamento" },
+                values: new object[] { 1, "Principal", "Rua Alcantara", 20, 40, "-23.519502072656618", "-46.59639509306988", "113", "Guilherme Cotching" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "semaforos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
